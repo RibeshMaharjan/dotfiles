8f979bfa4e531821101be9ba62988c8fe1876db8
Asura Mhzan <ribubaucha@gmail.com>
2026-08-07 06:11:43 +0000

    feat: search nvidia drive in genric path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,6 @@
 starship init fish | source
 
 # Auto-select NVIDIA GPU for Zed if available
-if lspci -nn | grep -q "10de:28e1"
-    set -x ZED_DEVICE_ID 0x28e1
+if grep -q "0x10de" /sys/bus/pci/devices/*/vendor
+   # NVIDIA settings
 end
-
-# pnpm
-set -gx PNPM_HOME "/home/asura/.local/share/pnpm"
-if not string match -q -- "$PNPM_HOME/bin" $PATH
-  set -gx PATH "$PNPM_HOME/bin" $PATH
-end
-# pnpm end
