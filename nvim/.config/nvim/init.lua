27bea6574173ebb75ec4cfb2ab6cf9e0a96d7d55
Asura Mhzan <ribubaucha@gmail.com>
2026-08-21 10:03:12 +0000

    refactor(nvim): move opts to options.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,3 @@
--- Enable line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Tabs and indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
--- Search settings
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
 require("keymaps")
 require("options")
 require("config.lazy")
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tabs and indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+
