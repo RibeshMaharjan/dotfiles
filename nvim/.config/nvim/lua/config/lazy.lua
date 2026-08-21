dba34ce230ec8e159b378ab8fb72c76ebc5bc29c
Asura Mhzan <ribubaucha@gmail.com>
2026-08-21 05:14:37 +0000

    feat(nvim): setup telescope and theme(catppuccin mocha)

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,20 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-  }
+    { import = "plugins" },
+  },
+
+  install = {
+    colorscheme = { "habamax" },
+  },
+
+  checker = {
+    enabled = true,
+  },
+  
+  performance = {
+    rtp = {
+      reset = false,
+    },
+  },
 })
