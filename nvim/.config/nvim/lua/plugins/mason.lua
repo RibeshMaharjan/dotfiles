ea7a18f928178e1abdc6b3589e4b3c1fe7e9e15d
Asura Mhzan <ribubaucha@gmail.com>
2026-08-22 14:19:37 +0000

    feat(nvim): enable lua lsp

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,42 +1,37 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("mason-lspconfig").setup {
-  --       ensure_installed = { "lua_ls", "tsc",  },
-  --       automatic_enable = false
-  --     }
-  --   end
-  --
-  --
-  --
   {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
         },
+        config = function()
+          require("mason").setup()
+        end,
       },
+      "neovim/nvim-lspconfig",
     },
     config = function()
-      require("mason").setup()
-    end,
+      require("mason-lspconfig").setup {
+        ensure_installed = {
+          "lua_ls",
+          "ts_lc",
+          "html",
+          "cssls",
+          "eslint",
+          "tailwindcss"
+        },
+        automatic_enable = false,
+      }
+    end
   },
-  -- {
-  --   "mason-org/mason-lspconfig.nvim",
-  --   opts = {},
-  --   dependencies = {
-  --     { "mason-org/mason.nvim", opts = {} },
-  --     "neovim/nvim-lspconfig",
-  --   },
-  --   config = function()
-  --     require("mason-lspconfig").setup {
-  --       ensure_installed = { "lua_ls", "ts_lc" },
-  --       automatic_enable = true
-  --     }
-  --   end
-  -- }, 
 }
