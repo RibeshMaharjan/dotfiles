84419a471f78b3c73805f765436736b759b0e672
Asura Mhzan <ribubaucha@gmail.com>
2026-08-22 15:52:26 +0000

    feat(nvim): isntall lazydev for lua

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -18,7 +18,20 @@ return {
           require("mason").setup()
         end,
       },
-      "neovim/nvim-lspconfig",
+      {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+          "folke/lazydev.nvim",
+          ft = "lua", -- only load on lua files
+          opts = {
+            library = {
+              -- See the configuration section for more details
+              -- Load luvit types when the `vim.uv` word is found
+              { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+          },
+        },
+      },
     },
     config = function()
       require("mason-lspconfig").setup {
