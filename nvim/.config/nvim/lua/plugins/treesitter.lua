6ab03951ad6f1c15d5b488bf0f7d31ac58249c52
Asura Mhzan <ribubaucha@gmail.com>
2026-08-23 08:28:15 +0000

    fix(nvim): lock nvim-treesitter to v0.9

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    -- Lock to the last stable release before the 0.12 rewrite
+    tag = 'v0.9.3',
     branch = "master",
     lazy = false,
     build = ':TSUpdate',
     config = function()
       require("nvim-treesitter.configs").setup({
         -- Parsers to automatically install
-        ensure_installed = { 
+        ensure_installed = {
           "lua",
           "vim",
           "vimdoc",
@@ -19,13 +21,23 @@ return {
           "markdown",
           "markdown_inline",
         },
-        
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = false,
+
+        -- List of parsers to ignore installing (or "all")
+        ignore_install = { "" },
+
         -- Enable Treesitter syntax highlighting
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false, -- Disables default Vim regex highlighting for better speed
         },
-        
+
         -- Enable Treesitter-based experimental indentation
         indent = {
           enable = true,
