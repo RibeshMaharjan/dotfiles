35420da7b05d9020b4b525ebf8f361849fc93696
Asura Mhzan <ribubaucha@gmail.com>
2026-08-26 04:40:39 +0000

    chore(nvim): stop tracking lazy lock file

--- nvim/.config/nvim/lua/keymap-plugins.lua
+++ nvim/.config/nvim/lua/keymap-plugins.lua
@@ -5,8 +5,13 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>en", function()
+  builtin.find_files {
+    cmd = vim.fn.stdpath('config')
+  }
+end, { desc = "Find Neovim Config" })
 
--- NeoTree
+-- neoTree
 
 -- Open / focus Neo-tree
 vim.keymap.set("n", "<C-n>", "<Cmd>Neotree filesystem reveal left<CR>")
--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -2,11 +2,20 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This ensures hidden files are shown by default
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
-    lazy = false, -- neo-tree will lazily load itself
+    lazy = false,                    -- neo-tree will lazily load itself
   }
 }
