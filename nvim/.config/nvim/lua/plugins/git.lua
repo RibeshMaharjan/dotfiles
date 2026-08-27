e9d84370f75f9c98cbd61fe2f77e80e62ef94f32
Asura Mhzan <ribubaucha@gmail.com>
2026-08-27 19:17:46 +0000

    feat(nvim): install lazygit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,3 +1,41 @@
 return {
-  "lewis6991/gitsigns.nvim",
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
+      local set = vim.keymap.set
+      gitsigns.setup()
+
+      set("n", "]c", function()
+        gitsigns.nav_hunk("next")
+      end, { desc = "Move to next hunk" })
+
+      set("n", "[c", function()
+        gitsigns.nav_hunk("prev")
+      end, { desc = "Move to previous hunk" })
+
+      set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+      set("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+    end,
+  },
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -29,24 +29,26 @@ return {
     config = function(_, opts)
       local builtin = require("telescope.builtin")
       local telescope = require("telescope")
-
+      local set = vim.keymap.set
       telescope.setup(opts)
 
       -- Keybinds
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-      vim.keymap.set("n", "<leader>en", function()
+      set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      set("n", "<leader>en", function()
         builtin.find_files({
           cwd = vim.fn.stdpath("config"),
         })
       end, { desc = "Find Neovim Config" })
-      vim.keymap.set("n", "<leader>ep", function()
+      set("n", "<leader>ep", function()
         builtin.find_files({
           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
         })
       end, { desc = "Search Neovim Package" })
+
+      set("n", "<leader>cs", builtin.lsp_document_symbols, { desc = "Document Symbols" })
 
       -- Extensions
       require("telescope").load_extension("fzf")
